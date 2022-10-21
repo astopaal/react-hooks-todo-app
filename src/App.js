e40904aaeaa56caab79ad3af357bbc9072272fd7
abdussamet topal <85809119+astopaal@users.noreply.github.com>
2022-10-21 16:04:20 +0300

    adding elements to list and listing them as something to do

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+import {
+  ChakraProvider,
+  Input,
+  Button,
+  List,
+  ListItem,
+  ListIcon,
+} from "@chakra-ui/react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ChakraProvider>
+      <div className="mainProvider">
+        <Input
+          value={todo}
+          onChange={e => setTodo(e.target.value)}
+          className="input"
+          variant="outline"
+          htmlSize={50}
+          width="auto"
+          placeholder="enter something to do..."
+        />
+
+        <div className="buttonGroup">
+          
+          <Button
+            onClick={() => {
+              setTodos([...todos, todo]);
+            }}
+            className="buttons"
+            colorScheme="green"
+          >
+            Add
+          </Button>
+          <Button className="buttons" colorScheme="blue">
+            Clear
+          </Button>
+          <Button className="buttons" colorScheme="red">
+            Clear Completed
+          </Button>
+          <Button className="buttons" colorScheme="yellow">
+            Edit
+          </Button>
+        </div>
+        <List
+          spacing={3}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            padding: "15px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {todos.map(todo => (
+          <ListItem textColor="white">
+            <ListIcon color="green.500" />
+            {todo}
+          </ListItem>
+          ))}
+          <ListItem textColor="white">
+            <ListIcon color="green.500" />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit
+          </ListItem>
+          <ListItem textColor="white">
+            <ListIcon color="green.500" />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit
+          </ListItem>
+        </List>
+      </div>
+    </ChakraProvider>
   );
 }
 
