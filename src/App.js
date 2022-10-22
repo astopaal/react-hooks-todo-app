64e18ba1d6f68fce8f9c0da3c15252181eec0fb7
abdussamet topal <85809119+astopaal@users.noreply.github.com>
2022-10-22 20:22:20 +0300

    some logical changes

--- src/App.js
+++ src/App.js
@@ -3,56 +3,46 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 
-import {
-  ChakraProvider,
-  Input,
-  Button,
-  List,
-  ListItem,
-  ListIcon,
-  Badge,
-} from "@chakra-ui/react";
+import { Input, Button, List, ListItem } from "@chakra-ui/react";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState({ id: 1, text: "Learn React" });
-  return (
-    <ChakraProvider>
-      <div className="mainProvider">
-        <Input
-          value={todo.text}
-          onChange={(e) =>
-            setTodo({
-              id: todos.length + 1,
-              text: e.target.value,
-            })
-          }
-          className="input"
-          variant="outline"
-          htmlSize={50}
-          width="auto"
-          placeholder="enter something to do..."
-        />
+  const [todo, setTodo] = useState({ id: 1, text: "" });
 
-        <div className="buttonGroup" key={todo.id}>
-          <Button
-            onClick={() => {
-              setTodos([todo, todos]);
-            }}
-            className="buttons"
-            colorScheme="green"
-          >
-            Add
-          </Button>
-          <Button className="buttons" colorScheme="blue">
-            Clear
-          </Button>
-          <Button className="buttons" colorScheme="red">
-            Clear Completed
-          </Button>
-          <Button className="buttons" colorScheme="yellow">
-            Edit
-          </Button>
+  const addTodo = () => {
+    if(todo.text === "") return;
+    setTodos((prevTodos) => [...prevTodos, todo]);
+    setTodo("");
+  };
+
+  return (
+    <>
+      <div className="mainProvider">
+        <div className="addingRow">
+          <Input
+            value={todo.text}
+            onChange={(e) => 
+              setTodo({
+                id: todos[todos.length - 1]?.id + 1 || 1,
+                text: e.target.value,
+              })
+            }
+            className="input"
+            variant="outline"
+            htmlSize={40}
+            width="auto"
+            placeholder="enter something to do..."
+          />
+
+          <div className="buttonGroup" key={todo.id}>
+            <Button
+              onClick={() => addTodo()}
+              className="buttons"
+              colorScheme="green"
+            >
+              Add
+            </Button>
+          </div>
         </div>
         <List
           spacing={3}
@@ -72,15 +62,18 @@ function App() {
                 border="solid"
                 borderColor="white"
                 borderWidth={1}
+                onClick={() => {
+                  setTodos(todos.filter((t) => t.id !== todo.id));
+                }}
               >
-                Edit
+                I did!
               </Button>
               {todo.text}
             </ListItem>
           ))}
         </List>
       </div>
-    </ChakraProvider>
+    </>
   );
 }
 
